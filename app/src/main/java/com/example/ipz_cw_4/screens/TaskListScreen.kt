a8713323d3a8a4807bad05de6ac9377655857936
Serhii Melnyk <serjeymelnik@gmail.com>
2024-03-14 14:38:46 +0200

    modify TaskListScreen for navigation

--- app/src/main/java/com/example/ipz_cw_4/screens/TaskListScreen.kt
+++ app/src/main/java/com/example/ipz_cw_4/screens/TaskListScreen.kt
@@ -14,15 +14,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 import com.example.ipz_cw_4.Task
 import com.example.ipz_cw_4.ui.theme.IPZ_CW_4_Serhii_MelnykTheme
 
 @Composable
-fun TaskListScreen(modifier: Modifier = Modifier) {
+fun TaskListScreen(modifier: Modifier = Modifier, navController: NavController) {
     val taskList =
         (1..100).map {
             Task("Task $it", "Description $it", listOf(true, false).random())
@@ -53,7 +55,7 @@ fun TaskListScreen(modifier: Modifier = Modifier) {
 @Composable
 fun TaskListScreen() {
     IPZ_CW_4_Serhii_MelnykTheme {
-        TaskListScreen()
+        TaskListScreen(navController = NavController(LocalContext.current))
     }
 }
 
