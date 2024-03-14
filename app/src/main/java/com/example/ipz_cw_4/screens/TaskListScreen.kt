f88f461c9840b100c213a4c8a037d4329d697a1e
Serhii Melnyk <serjeymelnik@gmail.com>
2024-03-14 14:17:23 +0200

    add list to TaskListScreen

--- app/src/main/java/com/example/ipz_cw_4/screens/TaskListScreen.kt
+++ app/src/main/java/com/example/ipz_cw_4/screens/TaskListScreen.kt
@@ -4,10 +4,13 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.ipz_cw_4.Task
 import com.example.ipz_cw_4.ui.theme.IPZ_CW_4_Serhii_MelnykTheme
 
 @Composable
 fun TaskListScreen(modifier: Modifier = Modifier) {
+    val taskList =
+        (1..100).map { Task("Task $it", "Description $it", listOf(true, false).random()) }
 
     LazyColumn {
 
